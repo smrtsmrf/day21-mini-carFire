a92ab0d8369ca01a2b305f7d636c1c4da86397cd
Candice Humpherys <smrtsmrf@gmail.com>
2016-07-14 09:41:56 -0600

    fixed firebase url to be the right project

--- js/carsService.js
+++ js/carsService.js
@@ -1,13 +1,13 @@
 angular.module('carFire').service('carsService', [function () {
 	this.getCars = function () {
-		return new Firebase('https://smrtsmrf-angular-seed.firebaseio.com' + '/cars');
+		return new Firebase('https://smrtsmrf-carfire.firebaseio.com/' + '/cars');
 	}
 
 	this.getCar = function (carId) {
-		return new Firebase('https://smrtsmrf-angular-seed.firebaseio.com' + '/cars/' + carId);
+		return new Firebase('https://smrtsmrf-carfire.firebaseio.com/' + '/cars/' + carId);
 	}
 
 	this.getComments = function (carId) {
-		return new Firebase('https://smrtsmrf-angular-seed.firebaseio.com' + '/comments/' + carId);
+		return new Firebase('https://smrtsmrf-carfire.firebaseio.com/' + '/comments/' + carId);
 	}
 }])
